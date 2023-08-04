7a5662424bb1a922f02e03ff391ae92323b497a8
riturajmahapatra <riturajmahapatra@gmail.com>
2023-08-04 13:47:23 +0530

    billboard-commit-1

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,9 @@
+const BillboardPage = async ({
+  params,
+}: {
+  params: { billboardId: string };
+}) => {
+  return <div>This the the add billboard page</div>;
+};
+
+export default BillboardPage;
