766b9546a99566552c751c763b84580a6e62d385
SinniS1 <sinniscode@gmail.com>
2023-08-08 12:01:39 +0530

    Dynamic routes complete

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,4 +1,5 @@
-import prismadb from "@/lib/prismadb";
+import prismadb from '@/lib/prismadb';
+import { Metadata } from 'next';
 
 interface DashboardPageProps {
   params: {
@@ -6,18 +7,16 @@ interface DashboardPageProps {
   };
 }
 
+export const metadata: Metadata = {
+  title: 'Overview'
+};
 
-
-const DashboardPage : React.FC<DashboardPageProps> = async({
-  params
-}) => {
-
-const store =await prismadb.store.findFirst({
-  where: {
-    id: params.storeId
-  }
-});
-
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId
+    }
+  });
 
   return <div>Active Store: {store?.name}</div>;
 };
