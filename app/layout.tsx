2412fec4bcd4f70b6f4f4dbba8fa1bb69a528ce7
SinniS1 <sinniscode@gmail.com>
2023-08-08 11:30:44 +0530

    Dynamic title bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,21 @@ import { ToasterProvider } from '@/providers/toast-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'Admin Dashboard',
-  description: 'Admin Dashboard'
+export const metadata = {
+  title: {
+    template: '%s : E-Commerce',
+    default: 'E-Commerce'
+  },
+  description:
+    'A E-commerce startup business, providing reliable solutions to the customers and vendors'
+  // openGraph: {
+  //   title: 'E-Commerce',
+  //   description: 'A startup business, providing solutions for website development, app development',
+  //   type: 'website',
+  //   site_name: 'E-Commerce',
+  //   url: 'https://digital-growth.vercel.app/',
+  //   images: '/DG_ShareImage.webp'
+  // }
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
