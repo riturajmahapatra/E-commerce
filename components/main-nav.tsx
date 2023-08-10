623f3eba07a7c1d368c73b22f59da14a649f7446
riturajmahapatra <riturajmahapatra@gmail.com>
2023-08-10 15:35:33 +0530

    BUG -FIXES

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -10,6 +10,11 @@ export function MainNav({ className, ...props }: React.HTMLAttributes<HTMLElemen
 
   const routes = [
     {
+      href: `/${params.storeId}`,
+      label: 'Overview',
+      active: pathname === `/${params.storeId}`
+    },
+    {
       href: `/${params.storeId}/billboards`,
       label: 'Billboards',
       active: pathname === `/${params.storeId}/billboards`
@@ -43,11 +48,6 @@ export function MainNav({ className, ...props }: React.HTMLAttributes<HTMLElemen
       href: `/${params.storeId}/settings`,
       label: 'Settings',
       active: pathname === `/${params.storeId}/settings`
-    },
-    {
-      href: `/${params.storeId}`,
-      label: 'Overview',
-      active: pathname === `/${params.storeId}`
     }
   ];
   return (
